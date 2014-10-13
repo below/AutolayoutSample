a6d901bb75c969cb0a1ad0fa0864a35bb5aa8f4e
Alexander von Below <Alex@vonBelow.Com>
2014-10-14 00:15:55 +0200

    Sample finished

--- AutolayoutSample/ViewController.swift
+++ AutolayoutSample/ViewController.swift
@@ -12,7 +12,43 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let redView = UIView()
+        redView.backgroundColor = UIColor.redColor()
+        redView.setTranslatesAutoresizingMaskIntoConstraints(false)
+        var lc : NSLayoutConstraint
+        self.view.addSubview(redView)
+        lc = NSLayoutConstraint(item: redView,
+            attribute: NSLayoutAttribute.Left,
+            relatedBy: NSLayoutRelation.Equal,
+            toItem: self.view, attribute: NSLayoutAttribute.Left,
+            multiplier: 1.0, constant: 12)
+        self.view.addConstraint(lc)
+        
+        
+        lc = NSLayoutConstraint(item: redView,
+            attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal,
+            toItem: self.topLayoutGuide,
+            attribute: NSLayoutAttribute.Bottom,
+            multiplier: 1.0, constant: 10)
+        
+        self.view.addConstraint(lc)
+        
+        
+        lc = NSLayoutConstraint(item:redView,
+            attribute: NSLayoutAttribute.Height,
+            relatedBy: NSLayoutRelation.Equal,
+            toItem: nil,
+            attribute: NSLayoutAttribute.NotAnAttribute,
+            multiplier: 1.0, constant: 24)
+        self.view.addConstraint(lc)
+        lc = NSLayoutConstraint(item:redView,
+            attribute: NSLayoutAttribute.Width,
+            relatedBy: NSLayoutRelation.Equal,
+            toItem: nil,
+            attribute: NSLayoutAttribute.NotAnAttribute,
+            multiplier: 1.0, constant: 24)
+        self.view.addConstraint(lc)
     }
 
     override func didReceiveMemoryWarning() {
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-AutolayoutSample
-================
+# AutolayoutSample
 
+This sample is to demonstrate how to create autolayout constraints which will position items so that they will adjust correctly both for the double-height "in-call" status bar, as well as for the new landscape mode in iOS 8.
+
+Maybe it is of use for someone
